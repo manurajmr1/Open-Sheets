c6fa1c3ebb1e2af7bbfd2b649f6002282a5346b4
Arun Thomas <arun@FTS-DSK-048>
2016-11-05 16:47:36 +0530

    commit

--- view.php
+++ view.php
@@ -339,7 +339,6 @@ var $ = function(id) {
                                 } else {
                                   setTotalValue(row, value,'buffered');
                                 }
-
                                 Handsontable.renderers.NumericRenderer
                                     .apply(
                                         this,
